3cc5cb1d73d0c29095c9bb4478d85f08e79507b7
Laboratorio <juan.patino@udla.edu.ec>
2023-11-08 15:07:53 -0500

    Segunda linea de codigo

---
+++ archivo.h
@@ -0,0 +1,4 @@
+#ifndef MACRO1
+#define MACRO1
+int suma(int a, int b);
+#endif 
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "archivo.h"
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    printf("Ingrese el 1er valor");
+    scanf("%d", &a);
+    printf("Ingrese el 2do valor");
+    scanf("%d", &b);
+    return 0;
+}
